5516efbe5f12bdea0485faf990e7837b00c8bbea
Yaroslav Yaroshevsky <americanrolling@gmail.com>
2020-01-18 02:49:56 +0700

    test1 > if > event 1 and 0 = return 1; else > for > n * n -1;

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,15 @@
  * @returns {number}
  */
 function factorial(n) {
-
+    let sum = 1;
+    
+    if ( n == 1 || n == 0 ) {
+        return sum;
+    }
+    else {
+        for ( let i = n; i != 1; --i) {
+            sum *= i;
+        }
+        return sum;
+    }
 }
