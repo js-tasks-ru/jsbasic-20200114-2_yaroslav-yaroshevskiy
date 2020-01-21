f77a6cfcfd56b1d3696d9b4a96fb37993bdd30bb
Yaroslav Yaroshevsky <americanrolling@gmail.com>
2020-01-21 23:07:41 +0700

    module1..task3: done

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,4 +4,8 @@
  * @returns {string}
  */
 function ucFirst(str) {
+  let result = str == undefined ? ' ' :
+               str.length == 1 ? str.toUpperCase() :
+               str[0].toUpperCase() + str.slice(1)
+  return result;
 }
