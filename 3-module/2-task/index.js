f6104c850845f5491b7809701e81714e3cfdb3e9
Yaroslav Yaroshevsky <americanrolling@gmail.com>
2020-01-28 21:26:50 +0700

    module3 task2 done

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,17 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-}
+  let minMax = str
+  // Экранирование символов для разделения строки на массивы
+    .split(/ |,/)
+  // Фильтруем содержимое массива на соответствие Number (Блять, это можно сделать проще? Выглядит пожеванно)
+    .filter( value => !isNaN(parseFloat(value)))
+  // Приводим к типу данных Number
+    .map(value => parseFloat(value));
+  // Возвращаем это убожество с использованием встроенного объекта Math.min\max
+  return {
+    min: Math.min.apply(null, minMax), 
+    max: Math.max.apply(null, minMax)
+  }
+    
+} 
